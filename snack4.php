bfd08d06b97acfb114dc42207f4ba8a8ce79a7a8
Samuel Scozzari <s_scozzari@libero.it>
2022-02-15 16:01:38 +0100

    code cleanup

--- snack4.php
+++ snack4.php
@@ -19,6 +19,25 @@
       $numbers[] = $randomArray[$i];
     } 
   }
-
-  var_dump($numbers)
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Snack 4</title>
+ </head>
+ <body>
+   <h2>Disposizione di 15 numeri randomici</h2>
+   <?php 
+    for($i= 0; $i < 15; $i++) {
+      if(!in_array($randomArray[$i], $numbers, true)) {
+        $numbers[] = $randomArray[$i];
+      } 
+      echo('<div>' . $numbers[$i] . '</div>');
+    }
+  ?>
+ </body>
+ </html>
